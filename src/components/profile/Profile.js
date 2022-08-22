5efde430bb99a759f58ee782611e86d48eb5fb76
1930346 <1930346@upv.edu.mx>
2022-08-22 11:45:54 -0500

    Update Profile.js

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,33 +1,15 @@
 import { useContext } from "react";
-import { useHistory } from "react-router-dom";
 import Header from "../common/Header";
-import * as firebaseService from "../../services/firebase";
-import * as uiService from "../../services/ui";
-import { useEffect, useState, useRef } from "react";
 
 
 import Context from "../../context";
-import { withRouter } from 'react-router-dom';
 
 const Profile = () => {
     
-    const [rideRequests, setRideRequests] = useState();
 
-    const { user, setCurrentRide, setSelectedFrom, setSelectedTo } =
+    const { user } =
       useContext(Context);
   
-    const fbUserRef = useRef(firebaseService.getRef("users"));
-
-    useEffect(() => {
-        firebaseService.getDataRealtimeQuery({
-          ref: fbUserRef,
-          query: "status",
-          criteria: "waiting",
-        });
-        return () => {
-          firebaseService.offRealtimeDatabase(fbUserRef.current);
-        };
-      }, []);
 
     const style = {
         width:"100%",
@@ -39,7 +21,6 @@ const Profile = () => {
         return (
             <div>
             <span>MedHelp</span>
-            <span> {user.fullname} </span>
             {/* {user && (
                 <div>
                   <img src={user.image} alt={user.email} />
