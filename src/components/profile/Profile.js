2a909186dcc19bd26ce82a085cc13e3e67432c44
1930346 <1930346@upv.edu.mx>
2022-08-22 10:29:29 -0500

    Update Profile.js

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -6,7 +6,7 @@ import { withRouter } from 'react-router-dom';
 
 const Profile = () => {
     
-    const { user, setUser, cometChat } = useContext(Context);
+    const { user, rides, cometChat } = useContext(Context);
     //Function to extract the number of user rides from the user object
     const getNumberOfRides = () => {
         let numberOfRides = 0;
@@ -16,28 +16,34 @@ const Profile = () => {
         return numberOfRides;
     };
 
+    const renderUserInfo = () => {
+        return (
+            <div className="profile__info">
+                <div className="profile__info__image">
+                    <img src={user.image} alt={user.email} />
+                </div>
+                <div className="profile__info__details">
+                    <div className="profile__info__details__name">
+                        <span>{user.fullname}</span>
+                    </div>
+                    <div className="profile__info__details__email">
+                        <span>{user.email}</span>
+                    </div>
+                    <div className="profile__info__details__rides">
+                        <span>{getNumberOfRides()}</span>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 
   return (
     <>
-        <Header />
-
+        <Header />  
+        <div className="profile__container"/>
+        {renderUserInfo()}
     </>
-    //giving a beautiful format as presentation of info and table of rides
 
-    // <div className="header">
-    //   <div className="header__left">
-    //     <span>MedHelp</span>
-    //     {user && (
-    //       <div className="header__right">
-    //         <img src={user.image} alt={user.email} />
-    //         <span>Hello, {user.fullname}</span>
-    //       </div>
-    //     )}
-    //   </div>
-    //   <span className="header__logout" onClick={logout}>
-    //     <span>Logout</span>
-    //   </span>
-    // </div>
   );
 };
 export default Profile;
